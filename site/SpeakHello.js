c40cc06f35137e32e6fd3a2c9e21372dda99574c
Kateryna <kateryna@rambler.ru>
2016-08-28 14:01:39 +0300

    Message

---
+++ site/SpeakHello.js
@@ -0,0 +1,7 @@
+  var yaakovGreeter = {};
+  yaakovGreeter.name = "Yaakov";
+  yaakovGreeter.sayHello = function () {
+  console.log("Hello " + yaakovGreeter.name);
+  }
+
+ 
---
+++ site/script.js
@@ -0,0 +1,5 @@
+ yaakovGreeter.sayHello();
+ yaakovGreeter.sayBye()
+
+
+
